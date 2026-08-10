bc7230ef4a31fa71ab10eb2eec56816f42925ce6
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-08-10 18:49:55 +0300

    function to trait. looks cooler now

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,25 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
-
 #[derive(Debug)]
 struct TraceLine {
     pc: u64,
     taken: bool,
 }
 
-fn parse_trace_line(line: &str) -> TraceLine {
-    let mut parts = line.split_whitespace();
+impl TraceLine {
+    fn parse(line: &str) -> Self {
+        let mut parts = line.split_whitespace();
 
-    let pc_str = parts.next().expect("pc not a number");
-    let pc_str = pc_str.trim_start_matches("0x");
-    
-    let taken_str = parts.next().expect("pc not a number");
-    let pc = u64::from_str_radix(pc_str, 16).expect("pc is a bad number");
-    let taken = taken_str == "1";
+        let pc_str = parts.next().expect("pc not a number");
+        let pc_str = pc_str.trim_start_matches("0x");
 
-    TraceLine { pc, taken }
+        let taken_str = parts.next().expect("pc not a number");
+        let pc = u64::from_str_radix(pc_str, 16).expect("pc is a bad number");
+        let taken = taken_str == "1";
+
+        TraceLine { pc, taken }
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -29,7 +30,7 @@ fn main() -> io::Result<()> {
 
     for line in reader.lines() {
         let line = line?;
-        let trace_line = parse_trace_line(&line);
+        let trace_line = TraceLine::parse(&line);
         println!("{:?}", trace_line);
     }
     Ok(())
