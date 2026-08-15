31d842afa8b44757a45322f1bdf8764de11635bb
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-08-15 14:59:47 +0300

    base predictor predict() and update() funcs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
-
-const BASE_PREDICTOR_SIZE = 200;
+const BASE_PREDICTOR_SIZE: usize = 4096;
 
 #[derive(Debug)]
 struct TraceLine {
@@ -26,20 +25,54 @@ impl TraceLine {
     }
 }
 
-
-
-struct Tage { 
-    base_predictor: Vec<u8>,
+struct Tage {
+    base_predictor: Vec<u16>,
 }
 
-impl Tage { 
-    fn new() -> Self { 
-            Tage { base_predictor: vec![2, BASE_PREDICTOR_SIZE] }
+#[derive(Clone, Copy)]
+struct PredictionResult {
+    taken: bool,
+    t0_index: usize,
+}
+impl Tage {
+    fn new() -> Self {
+        Tage {
+            base_predictor: vec![2; BASE_PREDICTOR_SIZE],
+        }
+    }
+    fn predict_base(&self, index: usize) -> bool {
+        let prediction = self.base_predictor[index];
+        return prediction > 0b10;
+    }
+    fn get_t0_index(pc: u64) -> usize {
+        (pc & 0xFFF) as usize // get last 12 bit
+    }
+    fn predict(&self, trace_line: &TraceLine) -> PredictionResult {
+        let index = Tage::get_t0_index(trace_line.pc);
+
+        PredictionResult {
+            t0_index: index,
+            taken: self.predict_base(index),
+        }
+    }
+    fn learn(&mut self, meta: PredictionResult, actual_result: bool) {
+        let counter = &mut self.base_predictor[meta.t0_index];
+
+        if actual_result {
+            if *counter < 3 {
+                *counter += 1;
+            }
+        } else {
+            if *counter > 0 {
+                *counter -= 1;
+            }
+        }
     }
 }
 
-
 fn main() -> io::Result<()> {
+    let tage = Tage::new();
+
     let path = Path::new("traces/trace_01");
     let file = File::open(path)?;
     let reader = BufReader::new(file);
@@ -48,6 +81,7 @@ fn main() -> io::Result<()> {
         let line = line?;
         let trace_line = TraceLine::parse(&line);
         println!("{:?}", trace_line);
+        println!("{:?}", tage.predict(&trace_line));
     }
     Ok(())
 }
